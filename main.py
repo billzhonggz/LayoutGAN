9d6c5fa2ba1c2731c9bf20f1326a8a09a2c1f131
Bill ZHONG <billzhonggz@outlook.com>
2019-04-21 12:01:31 +0800

    New branch: to verify the idea instead of focusing engineering stuff.

--- main.py
+++ main.py
@@ -1,107 +1,16 @@
-"""Entrance of this LayoutGAN implementation.
-
-This file handles command-line calls with arguments.
-
-List of available arguments and options.
---train --dataset-path --output-model-path --number-of-epoch trigger a training process.
---evaluate --pretrained-model-path --output-data-path
-
-©2019-current, Junru Zhong, all rights reserved.
-"""
-
-import sys
-import getopt
-
-long_args = [
-    'help',
-    'train',
-    'dataset-path=',
-    'output-model-path=',
-    'number-of-epoch=',
-    'evaluate',
-    'pretrained-model-path=',
-    'output-data-path='
-]
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
 
 
-def usage():
-    print('Usage: \n' + sys.argv[0] +
-          ' --train --dataset-path= --output-model-path= --number-of-epoch= trigger a training process.\n' +
-          sys.argv[0] + ' --evaluate --pretrained-model-path= --output-data-path= to start a evaluation.')
+# Root directory for dataset.
+dataroot = "data/mnist"
 
+# Number of workers for dataloader
+workers = 2
 
-def train(optlist):
-    """Process arguments when training mode is triggered.
-    Report errors when
-    1. Missing arguments.
-    2. Path provided is invalid.
-    """
-    dataset_path = ''
-    output_model_path = ''
-    number_of_epoch = ''
-    for opt, arg in optlist:
-        if opt == '--dataset-path':
-            dataset_path = arg
-        if opt == '--output-model-path':
-            output_model_path = arg
-        if opt == '--number-of-epoch':
-            number_of_epoch = arg
-    if dataset_path or output_model_path or number_of_epoch is '':
-        print('Missing arguments for training.')
-        usage()
-        sys.exit(2)
-    else:
-        # TODO: Start a train.
-        print(dataset_path + '\n' + output_model_path + '\n' + number_of_epoch)
+# Batch size during training
+batch_size = 128
 
-
-def evaluate(optlist):
-    """Process arguments when evaluation mode is triggered.
-    Report errors when
-    1. Missing arguments.
-    2. Path provided is invalid.
-    """
-    pretrained_model_path = ''
-    output_data_path = ''
-    for opt, arg in optlist:
-        if opt == '--pretrained-model-path':
-            pretrained_model_path = arg
-        if opt == '--output-model-path':
-            output_data_path = arg
-    if pretrained_model_path or output_data_path is '':
-        print('Missing arguments for evaluation.')
-        usage()
-        sys.exit(2)
-    else:
-        # TODO: Start a evaluate.
-        print(pretrained_model_path + '\n' + output_data_path)
-
-
-def main():
-    """The main function.
-    Read the arguments, and direct the program accordingly.
-    Report errors when any invalid argument is given.
-    """
-    try:
-        (optlist, args) = getopt.getopt(sys.argv[1:], '', long_args)
-        # print(optlist)
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print(err)  # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
-
-    # Analysis the arguments and trigger functions.
-    for opt, arg in optlist:
-        if opt == '--help':
-            usage()
-            sys.exit(0)
-        elif opt == '--train':
-            print('Enter training mode.')
-            train(optlist)
-        elif opt == '--evaluate':
-            print('Enter evaluate mode.')
-
-
-if __name__ == "__main__":
-    main()
