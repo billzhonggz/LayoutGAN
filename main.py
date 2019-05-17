29402e3141750fa4e58f1f278e70563ef299a70a
Bill ZHONG <billzhonggz@outlook.com>
2019-05-17 15:44:21 +0800

    main.py: Transferred the single image from greyscale values to class number.

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 
 # Enable eager execution. Not necessary for TensorFlow 2.0+.
 # tf.enable_eager_execution()
 
-def load_mnist():
+def load_mnist(thresh=200):
     """This function loads the MNIST dataset from TensorFlow official Datasets."""
     # Download from Google
     mnist_builder = tfds.builder('mnist')
@@ -24,11 +26,21 @@ def load_mnist():
     # Transfer to TensorFlow `Dataset` object.
     datasets = mnist_builder.as_dataset()
     train_dataset = datasets['train']
-    print(isinstance(train_dataset, tf.data.Dataset))
+    # print(isinstance(train_dataset, tf.data.Dataset))
     # Transfer MNIST images to tensors with geometry information in the iterator.
     iterator = tf.compat.v1.data.make_one_shot_iterator(train_dataset)
     # Get one element from the iterator.
     element = iterator.get_next()['image']
+    # Pick the pixels (as a 2D numpy array).
+    pixels = element.numpy()[:, :, 0]
+    layout_vector = np.zeros((28, 28))
+    for i in range(0, 28):
+        for j in range(0, 28):
+            if pixels[i, j] >= thresh:
+                layout_vector[i, j] = 1
+            else:
+                layout_vector[i, j] = 0
+    # print(layout_vector)
 
 
 if __name__ == '__main__':
