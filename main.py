c56714c32c144e929c6c43208f8bd3ab9d7a7d85
Bill ZHONG <billzhonggz@outlook.com>
2019-05-18 17:40:21 +0800

    main.py: Finish the generator structure (except relation module).

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """Just another implementation on LayoutGAN.
-Use TensorFlow, TensorFlow-Graphics
+Use TensorFlow, TensorFlow-Graphics, and Keras
 
 Copyright ©2019-current, Junru Zhong, all rights reserved.
 """
@@ -7,10 +7,12 @@ Copyright ©2019-current, Junru Zhong, all rights reserved.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-import tensorflow_datasets as tfds
 import numpy as np
 
-from tensorflow.keras import datasets, layers, models
+# In TensorFlow 2.0.0a0, using tensorflow.keras will stop code completion, use keras instead.
+# Change back to tensorflow.keras before run, I believe it just a problem in __init__.py file.
+from keras import datasets, layers, models
+from keras import backend as K
 
 
 # Enable eager execution. Not necessary for TensorFlow 2.0+.
@@ -45,31 +47,96 @@ from tensorflow.keras import datasets, layers, models
 
 
 def transfer_greyscale_class(greyscale, thresh=200):
+    """For MNIST dataset only. Transfer pixel values to geometry points with classes."""
     if greyscale >= thresh:
         return 1
     else:
         return 0
 
 
-feature_size = 128  # TODO: change later.
-
-
-# The relation module.
 class RelationModule(layers.Layer):
-    """The relation module."""
+    """Compute the relations between the elements.
+    The module here has a same shape of inputs and outputs.
+    """
+
+    def __init__(self, num_classes, num_geometry_parameters, num_elements):
+        """Layer variables
+        W_r: weight matrix creates linear embeddings and context residual information.
+        N: the number of elements.
+        W_psi: a representation of features of element i.
+        W_phi: a representation of features of element j.
+        """
+        self.num_classes = num_classes
+        self.num_geometry_parameters = num_geometry_parameters
+        self.num_elements = num_elements
+        self.feature_size = self.num_classes + self.num_geometry_parameters
+
+        self.w_r = K.placeholder(shape=[self.feature_size, self.feature_size])
+        self.w_psi = layers.Dense(self.feature_size)
+        self.w_phi = layers.Dense(self.feature_size)
+
+        super(RelationModule, self).__init__()
+
+    def call(self, inputs, **kwargs):
+        """Forward computations
+        U: an unary function computes a representation of the embedded feature for element j.
+        H: a dot-product to compute a scalar value on the representations of element i and j.
+        Output = W_r\frac{1}{N}\sum_{\forall{j \neq i}}^{} H(f(p_i,\theta_i),f(p_j,\theta_j))U(f(p_j,\theta_j))+f(p_i, \theta_i)
+        """
+        pass
+
+    def compute_output_shape(self, input_shape):
+        """Same shape as input"""
+        return input_shape
+
+
+class Generator:
 
     def __init__(self):
-        super(RelationModule, self).__init__()
-        unary = layers.Dense(feature_size * 2 * 2, input_shape=(feature_size * 2 * 2,))
-        psi = self.add_weight(shape=())
+        self.num_elements = 128
+        self.learning_rate = 0.00002
+        self.beta1 = 1
+        self.beta2 = 1
+        self.num_class = 2  # For MNIST
+        self.num_geometry_parameter = 2  # For MNIST
+
+    def build_generator(self):
+        feature_size = self.num_class + self.num_geometry_parameter
+
+        # The generator
+        generator = models.Sequential()
+
+        # Encoder fully connected layers.
+        generator.add(layers.Dense(feature_size * 2, input_shape=(feature_size,)))
+        generator.add(layers.BatchNormalization())
+        generator.add(layers.Dense(feature_size * 2 * 2))
+        generator.add(layers.BatchNormalization())
+        # TODO: Relation module.
+        generator.add(layers.Dense(feature_size * 2 * 2))
+        generator.add(RelationModule(self.num_class, self.num_geometry_parameter, self.num_elements))
+        # Decoder fully connected layers.
+        generator.add(layers.Dense(feature_size * 2))
+        generator.add(layers.BatchNormalization())
+        generator.add(layers.Dense(feature_size))
+        generator.add(layers.BatchNormalization())
+        # Branches
+        generator.add(layers.Dense(self.num_class))
+        generator.add(layers.Dense(self.num_geometry_parameter))
+
+        generator.summary()
 
 
-# The generator
-generator = models.Sequential()
-generator.add(layers.Dense(feature_size * 2, input_shape=(feature_size,)))  # layers.Dense(output_shape, input_shape=?)
-generator.add(layers.Dense(feature_size * 2 * 2))
-generator.add(layers.Dense(feature_size * 2 * 2))
-generator.add(RelationModule)
+class Discriminator:
+
+    def __init__(self):
+        pass
+
+    def build_relational_discriminator(self):
+        pass
+
+    def build_wireframe_discriminator(self):
+        pass
+
 
 if __name__ == '__main__':
     print(tf.keras.__version__)
